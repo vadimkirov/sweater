d637a97f86fc273f39cef3af96e308d20848d397
vadimkirov <vadimkirov@yandex.ru>
2020-09-07 19:42:38 +0300

    basic security

--- src/main/java/org/example/sweater/domain/Role.java
+++ src/main/java/org/example/sweater/domain/Role.java
@@ -3,7 +3,8 @@ package org.example.sweater.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER,
+    ADMIN;
 
     @Override
     public String getAuthority() {
