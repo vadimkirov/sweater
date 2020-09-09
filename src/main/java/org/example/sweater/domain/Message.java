97cd30529f28730d656754fcde444ae67eadc4bd
vadimkirov <vadimkirov@yandex.ru>
2020-09-09 17:48:15 +0300

    перед миграцией db

--- src/main/java/org/example/sweater/domain/Message.java
+++ src/main/java/org/example/sweater/domain/Message.java
@@ -7,7 +7,7 @@ public class Message {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
     private String text;
 
@@ -28,11 +28,11 @@ public class Message {
         this.tag = tag;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
